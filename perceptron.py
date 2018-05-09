fd30a3c5744e904f92a2db6144dbdce25d02c1df
RFrankowski <r.franek93@gmail.com>
2018-05-09 22:02:15 +0200

    naprawienie bledow

--- perceptron.py
+++ perceptron.py
@@ -46,32 +46,34 @@ class Perceptron:
             # print self.weigths
 
 
-
 # print p.guess([-5, 45])
 # and training data
-training_data = [([0, 0, 1], 0), ([0, 1, 1], 0), ([1, 0, 1], 0), ([1, 1, 1], 1)]
+training_data = [[[0, 0, 1], 0], [[0, 1, 1], 0], [[1, 0, 1], 0], [[1, 1, 1], 1]]
 
-training_data_or = [(([0, 0, 1]), 0), (([0, 1, 1]), 1), (([1, 0, 1]), 1), (([1, 1, 1]), 1), ]
+training_data_or = [[[0, 0, 1], 0], [[0, 1, 1], 1], [[1, 0, 1], 1], [[1, 1, 1], 1]]
 
-training_data_not = [([0], 1), ([1], 0)]
+training_data_not = [[[0], 1], [[1], 0]]
+
+
 # print p.guess([0, 0, 1])
 
 
 def ucz(per, tr_data):
+    # print tr_data
+    # print tr_data[2][1]
     for k in range(100):
         i = random.choice(tr_data)
         per.train(i[0], i[1])
 
-    print " tutaj powinno byc 0 " + str(per.guess([0, 0, 1]))
-    print " tutaj powinno byc 0 " + str(per.guess([0, 1, 1]))
-    print " tutaj powinno byc 0 " + str(per.guess([1, 0, 1]))
-    print " tutaj powinno byc 1 " + str(per.guess([1, 1, 1]))
+    for li in tr_data:
+        print " tutaj powinno byc  " + str(li[1]) + " a jest " + str(per.guess(li[0]))
+
 
 # perceptron dla and
-p = Perceptron()
-ucz(p, training_data)
-print p.lr
-print p.weigths
+# p = Perceptron()
+# ucz(p, training_data)
+# print p.lr
+# print p.weigths
 
 # gotowy perceptron do pokazania dla and
 # pAnd = Perceptron()
@@ -79,5 +81,14 @@ print p.weigths
 # pAnd.weigths = [0.469757865067298, 0.1820034389739238, -0.5845975069602078]
 # pAnd.lr = 0.752135511929
 # print pAnd.guess([1, 1, 1])
+
+
+# ptrainOr = Perceptron()
+# ucz(ptrainOr, training_data_or)
+# print ptrainOr.lr
+# print ptrainOr.weigths
+#
+# ptrainOr.lr = 0.167631213692
+# ptrainOr.weigths = [0.6586391948364184, 0.43477785666346414, -0.10141305826027047]
 
 
